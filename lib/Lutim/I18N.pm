11f7816b0b60442b586fcb01b0fbe743a877905e
Luc Didry <luc@framasoft.org>
2017-06-14 17:15:08 +0200

    Small changes
    - Use tag helpers
    - Fix indentation in I18N.pm

--- lib/Lutim/I18N.pm
+++ lib/Lutim/I18N.pm
@@ -4,7 +4,7 @@ package Lutim::I18N;
 use base 'Locale::Maketext';
 use File::Basename qw/dirname/;
 use Locale::Maketext::Lexicon {
-    _auto => 1,
+    _auto   => 1,
     _decode => 1,
     _style  => 'gettext',
     '*' => [
